56267ef2937c942b284b9a5ec6f09f900567e781
Syed Amaan Ali <amaan7733@Syeds-MacBook-Air.local>
2025-07-24 21:28:39 +0530

    Remove .env from repo

--- dist/main.js
+++ dist/main.js
@@ -6,13 +6,17 @@ const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
-        origin: ['http://localhost:3000', 'http://192.168.1.16:3000'],
+        origin: [
+            "http://localhost:3000",
+            "http://localhost:3002",
+            "http://192.168.1.16:3000",
+        ],
         credentials: true,
     });
     app.useGlobalPipes(new common_1.ValidationPipe());
-    app.setGlobalPrefix('api');
+    app.setGlobalPrefix("api");
     await app.listen(3001);
-    console.log('Backend server running on http://localhost:3001');
+    console.log("Backend server running on http://localhost:3001");
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
