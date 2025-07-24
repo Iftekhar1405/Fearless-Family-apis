0fc933178bae8c3b5d0b9feb09f15594248c6c0a
Syed Amaan Ali <amaan7733@Syeds-MacBook-Air.local>
2025-07-24 21:29:56 +0530

    feat : removed env and updated config

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,27 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // ✅ Enable CORS for any origin
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://192.168.1.16:3000'], // replace with your actual frontend IP(s)
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3002",
+      "http://192.168.1.16:3000",
+    ], // replace with your actual frontend IP(s)
     credentials: true,
   });
-
 
   // Enable validation pipes
   app.useGlobalPipes(new ValidationPipe());
 
   // Set global prefix
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix("api");
 
   await app.listen(3001);
-  console.log('Backend server running on http://localhost:3001');
+  console.log("Backend server running on http://localhost:3001");
 }
 bootstrap();
